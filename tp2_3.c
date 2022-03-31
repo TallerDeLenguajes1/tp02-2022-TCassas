1ef9b3f2e292f93c3c8e0a2a91fff87cd13fcb15
Tomás Cassas <tomascassas@gmail.com>
2022-03-31 19:17:57 -0300

    Punto 3 aritmetica de punteros

--- tp2_3.c
+++ tp2_3.c
@@ -5,11 +5,12 @@
 int main() {
     int i,j;
     int mt[N][M];
+    int * ptr = &mt;
 
-    for(i = 0;i<N; i++) {
-        for(j = 0;j<M; j++) {
-            mt[i][j] = 1 + rand() % 100;
-            printf("%d ", mt[i][j]);
+    for(i = 0; i < N; i++) {
+        for(j = 0; j < M; j++) {
+            *(ptr + (i * M + j)) = 1 + rand() % 100;
+            printf("%d\t", *(ptr + (i * M + j)));
         }
         printf("\n");
     }

e29113c4f520ddf152143ffa7987e0c667a74ba0
Tomás Cassas <tomascassas@gmail.com>
2022-03-31 17:41:42 -0300

    Commit inicial

---
+++ tp2_1_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define N 20
+
+int main() {
+    int i;
+    int vt[N];
+
+    for(i = 0 ; i < N; i++) {
+        vt[i] = 1 + rand() % 100;
+        printf("%d ", vt[i]);
+    }
+
+    return 0;
+}
